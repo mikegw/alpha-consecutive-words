8ab3c043de7c5a0ad92e387406a2519e953c516c
Mike <mike@cardflight.com>
2018-10-12 17:13:42 -0400

    Add solution

--- lib/sentence_finder.rb
+++ lib/sentence_finder.rb
@@ -1,6 +1,20 @@
 module SentenceFinder
-  def self.find(paragraph)
-    # Let's make this work!
-    paragraph
+  class << self
+    def find(paragraph)
+      sentences = paragraph.scan(/\w[^.?!]*[.?!]+/)
+      sentences.max_by { |sentence| alpha_consecutive_word_count(sentence) }
+    end
+
+    private
+
+    def alpha_consecutive_word_count(sentence)
+      words = sentence.split(' ')
+      words.count { |word| has_alpha_consecutive_letters?(word) }
+    end
+
+    def has_alpha_consecutive_letters?(word)
+      downcased = word.downcase
+      downcased.chars.any? { |char| downcased.include?(char.next) }
+    end
   end
 end
